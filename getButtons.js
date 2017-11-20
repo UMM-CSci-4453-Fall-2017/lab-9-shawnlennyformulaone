b054a7ad4735f42ade49287aaa98145e817b0e31
GLOBBIMUS <striker1342566@gmail.com>
2017-11-20 16:08:57 -0600

    Add timeStamp field to transactions
    Make it functional

--- getButtons.js
+++ getButtons.js
@@ -28,7 +28,7 @@ var selectButtonRecord = function(db){
   return query(mysql.format(sql));
 }
 
-var checkSupply = function(itemID, timestamp, db){
+var checkSupply = function(itemID, timeStamp, db){
   var sql = "select * from "+db+".supply where itemID = "+itemID+";"; //sql which checks if the item is in supply table
   var recordExists = false;
   var price;
@@ -46,16 +46,16 @@ var checkSupply = function(itemID, timestamp, db){
 })
 .then(function() {
   if(recordExists) { // If record exists then we call checkTransaction function
-    checkTransaction(itemID, price, itemName, db); // We also giving price, itemName, and id of the item as the arguments
+    checkTransaction(itemID, price, itemName, db, timeStamp); // We also giving price, itemName, and id of the item as the arguments
   }else{
     console.log("No item in supply with itemID "+itemID);
   }
   });
 }
 
-var checkTransaction = function(itemID, price, itemName, db){
+var checkTransaction = function(itemID, price, itemName, db, timeStamp){
   //This query will insert a record into transactions table if this item hasn't been add yet or just increment the quantity of that item if it was there already
-  var sql = "INSERT INTO " + db + ".transactions VALUES (" + itemID + ", " + "\"" +itemName + "\"" + ", " + 1 + ", " + price + ") ON DUPLICATE KEY UPDATE totalPrice = (quantity + 1)* " + price + " , quantity  = quantity+1;";
+  var sql = "INSERT INTO " + db + ".transactions VALUES (" + itemID + ", " + "\"" +itemName + "\"" + ", " + 1 + ", " + price + ", \"" + timeStamp + "\"" + ") ON DUPLICATE KEY UPDATE totalPrice = (quantity + 1)* " + price + " , quantity  = quantity+1, timeStamp = timeStamp;";
   query(mysql.format(sql));
 }
 
